43100d0d53071d8705552f539ffa11b6550d5842
josyoliveira <madsonsilva909@gmail.com>
2019-11-13 11:09:57 -0300

    adicionando o HTML para ser renderizado

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,10 +3,14 @@ package hello;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class GreetingController {
@@ -14,10 +18,18 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    // @RequestMapping("/greeting")
+    // public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    //     return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    // }
+
+
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public ModelAndView greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return new ModelAndView("/greeting.html");
     }
+
 
     @RequestMapping("/greeting/time")
     public Greeting greetingTime(@RequestParam(value = "hour", defaultValue = "0") int hour) {
